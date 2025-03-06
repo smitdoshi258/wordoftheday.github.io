be2dd7858b2c745dd173abece437271d71eeaea4
Smit Doshi <163644180+smitdoshi258@users.noreply.github.com>
2025-03-06 14:07:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,403 +1,44 @@
 const quotes = [{
-        name: "&#126 Smit Doshi",
-        quote: "&#x275D One who unites will make the world bright.&#x275E"
-    },
-    {
-    "name": "&#126 Steve Jobs",
-    "quote": "&#x275D The only way to do great work is to love what you do.&#x275E"
+    name: "Word: Serendipity",
+    quote: "Meaning:&#x275D The occurrence of events by chance in a happy or beneficial way &#x275E"
 },
 {
-    "name": "&#126 Nelson Mandela",
-    "quote": "&#x275D Education is the most powerful weapon which you can use to change the world.&#x275E"
+    name: "Word: Ephemeral",
+    quote: "Meaning:&#x275D Lasting for a very short time &#x275E"
 },
 {
-    "name": "&#126 Wayne Gretzky",
-    "quote": "&#x275D You miss 100% of the shots you don't take.&#x275E"
+    name: "Word: Quixotic",
+    quote: "Meaning:&#x275D Exceedingly idealistic; unrealistic and impractical &#x275E"
 },
 {
-    "name": "&#126 Albert Schweitzer",
-    "quote": "&#x275D Success is not the key to happiness. Happiness is the key to success.&#x275E"
+    name: "Word: Languid",
+    quote: "Meaning:&#x275D Displaying or having a disinclination for physical exertion or effort; slow and relaxed &#x275E"
 },
 {
-    "name": "&#126 Theodore Roosevelt",
-    "quote": "&#x275D Believe you can and you're halfway there.&#x275E"
+    name: "Word: Effervescent",
+    quote: "Meaning:&#x275D Bubbly, fizzy, or full of energy and enthusiasm &#x275E"
 },
 {
-    "name": "&#126 Eleanor Roosevelt",
-    "quote": "&#x275D The future belongs to those who believe in the beauty of their dreams.&#x275E"
+    name: "Word: Mellifluous",
+    quote: "Meaning:&#x275D Sweet or musical; pleasant to hear &#x275E"
 },
 {
-    "name": "&#126 Sam Levenson",
-    "quote": "&#x275D Don't watch the clock; do what it does. Keep going.&#x275E"
+    name: "Word: Obfuscate",
+    quote: "Meaning:&#x275D To render obscure, unclear, or unintelligible &#x275E"
 },
 {
-    "name": "&#126 Norman Vaughan",
-    "quote": "&#x275D Dream big and dare to fail.&#x275E"
+    name: "Word: Petrichor",
+    quote: "Meaning:&#x275D The pleasant, earthy scent after fresh rain &#x275E"
 },
 {
-    "name": "&#126 C.S. Lewis",
-    "quote": "&#x275D You are never too old to set another goal or to dream a new dream.&#x275E"
+    name: "Word: Sycophant",
+    quote: "Meaning:&#x275D A person who acts obsequiously toward someone important to gain advantage &#x275E"
 },
 {
-    "name": "&#126 C.S. Lewis",
-    "quote": "&#x275DHardships often prepare ordinary people for an extraordinary destiny.&#x275E"
-},
-{
-    "name": "&#126 Walt Whitman",
-    "quote": "&#x275D Keep your face always toward the sunshine—and shadows will fall behind you.&#x275E"
-},
-{
-    "name": "&#126 Peter Drucker",
-    "quote": "&#x275D The best way to predict the future is to create it.&#x275E"
-},
-{
-    "name": "&#126 William James",
-    "quote": "&#x275D Act as if what you do makes a difference. It does.&#x275E"
-},
-{
-    "name": "&#126 Henry David Thoreau",
-    "quote": "&#x275D Success usually comes to those who are too busy to be looking for it.&#x275E"
-},
-{
-    "name": "&#126 Napoleon Hill",
-    "quote": "&#x275D Don’t wait. The time will never be just right.&#x275E"
-},
-{
-    "name": "&#126 Zig Ziglar",
-    "quote": "&#x275D You don’t have to be great to start, but you have to start to be great.&#x275E"
-},
-{
-    "name": "&#126 Lao Tzu",
-    "quote": "&#x275D The journey of a thousand miles begins with one step.&#x275E"
-},
-{
-    "name": "&#126 Franklin D. Roosevelt",
-    "quote": "&#x275D The only limit to our realization of tomorrow is our doubts of today.&#x275E"
-},
-{
-    "name": "&#126 Mark Twain",
-    "quote": "&#x275D The secret of getting ahead is getting started.&#x275E"
-},
-{
-    "name": "&#126 Confucius",
-    "quote": "&#x275D It does not matter how slowly you go as long as you do not stop.&#x275E"
-},
-{
-    "name": "&#126 Ralph Waldo Emerson",
-    "quote": "&#x275D What lies behind us and what lies before us are tiny matters compared to what lies within us.&#x275E"
-},
-{
-    "name": "&#126 Aristotle",
-    "quote": "&#x275D Quality is not an act, it is a habit.&#x275E"
-},
-{
-    "name": "&#126 A.A. Milne",
-    "quote": "&#x275D You are braver than you believe, stronger than you seem, and smarter than you think.&#x275E"
-},
-{
-    "name": "&#126 Charles Kingsleigh",
-    "quote": "&#x275D The only way to achieve the impossible is to believe it is possible.&#x275E"
-},
-{
-    "name": "&#126 Winston Churchill",
-    "quote": "&#x275D Success is not final, failure is not fatal: It is the courage to continue that counts.&#x275E"
-},
-{
-    "name": "&#126 Nelson Mandela",
-    "quote": "&#x275D It always seems impossible until it's done.&#x275E"
-},
-{
-    "name": "&#126 Steve Jobs",
-    "quote": "&#x275D Your time is limited, don't waste it living someone else's life.&#x275E"
-},
-{
-    "name": "&#126 Walt Disney",
-    "quote": "&#x275D If you can dream it, you can do it.&#x275E"
-},
-{
-    "name": "&#126 Mahatma Gandhi",
-    "quote": "&#x275D You must be the change you wish to see in the world.&#x275E"
-},
-{
-    "name": "&#126 George Eliot",
-    "quote": "&#x275D It is never too late to be what you might have been.&#x275E"
-},
-{
-    "name": "&#126 Theodore Roosevelt",
-    "quote": "&#x275D Do what you can, with what you have, where you are.&#x275E"
-},
-{
-    "name": "&#126 George Addair",
-    "quote": "&#x275D Everything you’ve ever wanted is on the other side of fear.&#x275E"
-},
-{
-    "name": "&#126 Frank Sinatra",
-    "quote": "&#x275D The best revenge is massive success.&#x275E"
-},
-{
-    "name": "&#126 Franklin D. Roosevelt",
-    "quote": "&#x275D The only thing we have to fear is fear itself.&#x275E"
-},
-{
-    "name": "&#126 Ralph Waldo Emerson",
-    "quote": "&#x275D To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.&#x275E"
-},
-{
-    "name": "&#126 Albert Einstein",
-    "quote": "&#x275D In the middle of every difficulty lies opportunity.&#x275E"
-},
-{
-    "name": "&#126 Zig Ziglar",
-    "quote": "&#x275D What you get by achieving your goals is not as important as what you become by achieving your goals.&#x275E"
-},
-{
-    "name": "&#126 Ralph Waldo Emerson",
-    "quote": "&#x275D The only person you are destined to become is the person you decide to be.&#x275E"
-},
-{
-    "name": "&#126 Dalai Lama",
-    "quote": "&#x275D Happiness is not something ready made. It comes from your own actions.&#x275E"
-},
-{
-    "name": "&#126 Winston Churchill",
-    "quote": "&#x275D Success is walking from failure to failure with no loss of enthusiasm.&#x275E"
-},
-{
-    "name": "&#126 Charles R. Swindoll",
-    "quote": "&#x275D Life is 10% what happens to us and 90% how we react to it.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D Good things come to people who wait, but better things come to those who go out and get them.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D Just when the caterpillar thought the world was over, it became a butterfly.&#x275E"
-},
-{
-    "name": "&#126 Albert Einstein",
-    "quote": "&#x275D A person who never made a mistake never tried anything new.&#x275E"
-},
-{
-    "name": "&#126 Unknown",
-    "quote": "&#x275D The best way to succeed in this world is to act on the advice you give to others.&#x275E"
-},
-{
-    "name": "&#126 Christopher Columbus",
-    "quote": "&#x275D You can never cross the ocean until you have the courage to lose sight of the shore.&#x275E"
-},
-{
-    "name": "&#126 Ralph Waldo Emerson",
-    "quote": "&#x275D Do not go where the path may lead, go instead where there is no path and leave a trail.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D Don't limit your challenges. Challenge your limits.&#x275E"
-},
-{
-    "name": "&#126 Nelson Mandela",
-    "quote": "&#x275D The greatest glory in living lies not in never falling, but in rising every time we fall.&#x275E"
-},
-{
-    "name": "&#126 John F. Kennedy",
-    "quote": "&#x275D Every accomplishment starts with the decision to try.&#x275E"
-},
-{
-    "name": "&#126 Muhammad Ali",
-    "quote": "&#x275D Don't count the days, make the days count.&#x275E"
-},
-{
-    "name": "&#126 Maya Angelou",
-    "quote": "&#x275D You can’t use up creativity. The more you use, the more you have.&#x275E"
-},
-{
-    "name": "&#126 Thomas Paine",
-    "quote": "&#x275D The harder the conflict, the more glorious the triumph.&#x275E"
-},
-{
-    "name": "&#126 Plutarch",
-    "quote": "&#x275D What we achieve inwardly will change outer reality.&#x275E"
-},
-{
-    "name": "&#126 Booker T. Washington",
-    "quote": "&#x275D If you want to lift yourself up, lift up someone else.&#x275E"
-},
-{
-    "name": "&#126 Paulo Coelho",
-    "quote": "&#x275D Impossible is just an opinion.&#x275E"
-},
-{
-    "name": "&#126 William Shakespeare",
-    "quote": "&#x275D We know what we are, but know not what we may be.&#x275E"
-},
-{
-    "name": "&#126 Will Rogers",
-    "quote": "&#x275D Don’t let yesterday take up too much of today.&#x275E"
-},
-{
-    "name": "&#126 Jimmy Johnson",
-    "quote": "&#x275D The difference between ordinary and extraordinary is that little extra.&#x275E"
-},
-{
-    "name": "&#126 Ralph Waldo Emerson",
-    "quote": "&#x275D The only thing that can grow is the thing you give energy to.&#x275E"
-},
-{
-    "name": "&#126 Nido Qubein",
-    "quote": "&#x275D Your present circumstances don’t determine where you can go; they merely determine where you start.&#x275E"
-},
-{
-    "name": "&#126 Thomas Edison",
-    "quote": "&#x275D There is no substitute for hard work.&#x275E"
-},
-{
-    "name": "&#126 Arthur Ashe",
-    "quote": "&#x275D Start where you are. Use what you have. Do what you can.&#x275E"
-},
-{
-    "name": "&#126 Theodore Roosevelt",
-    "quote": "&#x275D Keep your eyes on the stars, and your feet on the ground.&#x275E"
-},
-{
-    "name": "&#126 William Butler Yeats",
-    "quote": "&#x275D Do not wait to strike till the iron is hot; but make it hot by striking.&#x275E"
-},
-{
-    "name": "&#126 John D. Rockefeller",
-    "quote": "&#x275D Don’t be afraid to give up the good to go for the great.&#x275E"
-},
-{
-    "name": "&#126 Thomas Jefferson",
-    "quote": "&#x275D I find that the harder I work, the more luck I seem to have.&#x275E"
-},
-{
-    "name": "&#126 Walter Elliot",
-    "quote": "&#x275D Perseverance is not a long race; it is many short races one after the other.&#x275E"
-},
-{
-    "name": "&#126 Milton Berle",
-    "quote": "&#x275D If opportunity doesn’t knock, build a door.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D The harder you work for something, the greater you'll feel when you achieve it.&#x275E"
-},
-{
-    "name": "&#126 Steve Jobs",
-    "quote": "&#x275D The only way to do great work is to love what you do.&#x275E"
-},
-{
-    "name": "&#126 Chris Grosser",
-    "quote": "&#x275D Opportunities don't happen. You create them.&#x275E"
-},
-{
-    "name": "&#126 Bo Bennett",
-    "quote": "&#x275D Success is not in what you have, but who you are.&#x275E"
-},
-{
-    "name": "&#126 H. Jackson Brown, Jr.",
-    "quote": "&#x275D The best preparation for tomorrow is doing your best today.&#x275E"
-},
-{
-    "name": "&#126 John Wooden",
-    "quote": "&#x275D Don't let what you cannot do interfere with what you can do.&#x275E"
-},
-{
-    "name": "&#126 J.K. Rowling",
-    "quote": "&#x275D It is our choices that show what we truly are, far more than our abilities.&#x275E"
-},
-{
-    "name": "&#126 Paulo Coelho",
-    "quote": "&#x275D When we strive to become better than we are, everything around us becomes better too.&#x275E"
-},
-{
-    "name": "&#126 Beverly Sills",
-    "quote": "&#x275D There are no shortcuts to any place worth going.&#x275E"
-},
-{
-    "name": "&#126 Albert Einstein",
-    "quote": "&#x275D Strive not to be a success, but rather to be of value.&#x275E"
-},
-{
-    "name": "&#126 William Arthur Ward",
-    "quote": "&#x275D If you can imagine it, you can achieve it. If you can dream it, you can become it.&#x275E"
-},
-{
-    "name": "&#126 Christian D. Larson",
-    "quote": "&#x275D Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D If you want to achieve greatness stop asking for permission.&#x275E"
-},
-{
-    "name": "&#126 Maya Angelou",
-    "quote": "&#x275D Nothing will work unless you do.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D Great things never came from comfort zones.&#x275E"
-},
-{
-    "name": "&#126 Vince Lombardi",
-    "quote": "&#x275D It’s not whether you get knocked down, it’s whether you get up.&#x275E"
-},
-{
-    "name": "&#126 Buddha",
-    "quote": "&#x275D What we think, we become.&#x275E"
-},
-{
-    "name": "&#126 Henry Ford",
-    "quote": "&#x275D Whether you think you can or you think you can’t, you’re right.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D Don’t stop when you’re tired. Stop when you’re done.&#x275E"
-},
-{
-    "name": "&#126 Robert Frost",
-    "quote": "&#x275D The best way out is always through.&#x275E"
-},
-{
-    "name": "&#126 Vidal Sassoon",
-    "quote": "&#x275D The only place where success comes before work is in the dictionary.&#x275E"
-},
-{
-    "name": "&#126 Marsha Sinetar",
-    "quote": "&#x275D Do what you love and the money will follow.&#x275E"
-},
-{
-    "name": "&#126 Colin Powell",
-    "quote": "&#x275D A dream doesn’t become reality through magic; it takes sweat, determination, and hard work.&#x275E"
-},
-{
-    "name": "&#126 Walt Disney",
-    "quote": "&#x275D The way to get started is to quit talking and begin doing.&#x275E"
-},
-{
-    "name": "&#126 Bill Cosby",
-    "quote": "&#x275D In order to succeed, your desire for success should be greater than your fear of failure.&#x275E"
-},
-{
-    "name": "&#126 Unknown",
-    "quote": "&#x275D You can’t fall if you don’t climb. But there’s no joy in living your whole life on the ground.&#x275E"
-},
-{
-    "name": "&#126 Anonymous",
-    "quote": "&#x275D The best view comes after the hardest climb.&#x275E"
-},
-{
-    "name": "&#126 Eleanor Roosevelt",
-    "quote": "&#x275D The purpose of life is to live it, to taste experience to the utmost, to reach out eagerly and without fear for newer and richer experience.&#x275E"
-},
-{
-    "name": "&#126 Steve Jobs",
-    "quote": "&#x275D Don’t let the noise of others’ opinions drown out your own inner voice.&#x275E"
-},
-{
-    "name": "&#126 Albert Einstein",
-    "quote": "&#x275D We cannot solve our problems with the same thinking we used when we created them.&#x275E"
+    name: "Word: Vestige",
+    quote: "Meaning:&#x275D A trace of something that is disappearing or no longer exists &#x275E"
 }
+
 
 ]
 const quoteBtn = document.querySelector('#quoteBtn');
